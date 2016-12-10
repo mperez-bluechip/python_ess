131e8514ca140128367ae53f2b6766695de6002e
Manny Perez <mannyperez@Mannys-MacBook-Pro.local>
2016-12-10 12:39:31 -0600

    Added and modified object type file

--- syntax-objects.py
+++ syntax-objects.py
@@ -3,7 +3,17 @@
 # This is an exercise file from Python 3 Essential Training on lynda.com
 # Copyright 2010 The BearHeart Group, LLC
 
+class Egg:
+    def __init__(self, kind = 'fried'):
+        self.kind = kind
+
+    def whatKind(self):
+        return self.kind
+
 def main():
-    
+    fried = Egg()
+    scrambled = Egg('scrambled')
+    print(fried.whatKind())
+    print(scrambled.whatKind())
 
 if __name__ == "__main__": main()
